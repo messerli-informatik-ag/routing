f400f5d431539cbb62a4d081af46e407d603183a
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-10-03 18:30:42 +0200

    Add basic future-proof builder

---
+++ Routing/RouteRegistryBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Routing
+{
+    public class RouteRegistryBuilder<TRequest, TResponse>
+    {
+        private readonly Func<TRequest, TResponse> _handleFallbackRequest;
+
+        private RouteRegistryBuilder(Func<TRequest, TResponse> handleFallbackRequest)
+        {
+            _handleFallbackRequest = handleFallbackRequest;
+        }
+
+        public RouteRegistryBuilder<TRequest, TResponse> WithFallbackRequestHandler(
+            Func<TRequest, TResponse> handleFallbackRequest)
+        {
+            return new RouteRegistryBuilder<TRequest, TResponse>(handleFallbackRequest);
+        }
+
+        public IRouteRegistry<TRequest, TResponse> Build() =>
+            new RouteRegistryFacade<TRequest, TResponse>(_handleFallbackRequest);
+    }
+}
