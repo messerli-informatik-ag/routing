45af2b7dc4fef746804739dce86af409412e467d
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2019-10-09 16:59:04 +0200

    Trim trailing slashes

--- Routing/Parsing/PathParsing.cs
+++ Routing/Parsing/PathParsing.cs
@@ -15,7 +15,7 @@ namespace Messerli.Routing.Parsing
             }
 
             const int lengthOfRoot = 1;
-            var trimmedPath = path.Substring(lengthOfRoot);
+            var trimmedPath = path.Substring(lengthOfRoot).TrimEnd(SegmentDelimiterToken);
             return trimmedPath.Any()
                 ? trimmedPath.Split(SegmentDelimiterToken)
                 : new string[0];
