10382446c1bca10d4865066f6d252143a2d86c22
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2019-10-09 16:08:13 +0200

    Test that exceptions are propagated

--- Routing.AspNetCore.Test/MiddlewareTest.cs
+++ Routing.AspNetCore.Test/MiddlewareTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +14,8 @@ namespace Messerli.Routing.AspNetCore.Test
     {
         private const string FallbackResponse = "not found";
 
+        private const string ErrorRoute = "/error";
+
         [Fact]
         public async Task FallbackRouteIsCalled()
         {
@@ -21,6 +25,19 @@ namespace Messerli.Routing.AspNetCore.Test
                 var response = await client.GetAsync("/");
                 var responseBody = await response.Content.ReadAsStringAsync();
                 Assert.Equal(FallbackResponse, responseBody);
+            }
+        }
+
+        [Fact]
+        public async Task ErrorsArePropagated()
+        {
+            using (var server = CreateTestServer())
+            using (var client = server.CreateClient())
+            {
+                await Assert.ThrowsAsync<CustomException>(async () =>
+                {
+                    await client.GetAsync(ErrorRoute);
+                });
             }
         }
 
@@ -34,10 +51,20 @@ namespace Messerli.Routing.AspNetCore.Test
                 var routeRegistry = RouteRegistryBuilder<HttpContext, string>
                     .WithFallbackRequestHandler(HandleFallbackRequest)
                     .Build();
+
+                RegisterErrorRoute(routeRegistry);
+
                 app.UseRouting(
                     routeRegistry,
                     Identity,
                     ApplyResponseToContext);
+            }
+
+            private static void RegisterErrorRoute(IRouteRegistry<HttpContext, string> routeRegistry)
+            {
+                routeRegistry.Register(
+                    new Endpoint(HttpMethod.Get, ErrorRoute),
+                    (context, parameters) => throw new CustomException());
             }
 
             private static T Identity<T>(T value) =>
@@ -48,6 +75,10 @@ namespace Messerli.Routing.AspNetCore.Test
 
             private static string HandleFallbackRequest(HttpContext request) =>
                 FallbackResponse;
+        }
+
+        private class CustomException : Exception
+        {
         }
     }
 }
